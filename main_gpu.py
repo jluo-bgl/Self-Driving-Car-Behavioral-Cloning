339bd2a560bc8a04bb8ac7aea487b5c6d0a0a745
James Luo <jamesluo.au@gmail.com>
2017-01-26 08:24:15 +1100

    use of all centre image still able to drive whole lap

--- main_gpu.py
+++ main_gpu.py
@@ -6,8 +6,8 @@ from trainer import Trainer
 data_set = DriveDataSet.from_csv("datasets/udacity-sample-track-1/driving_log.csv", crop_images=True,
                                  filter_method=drive_record_filter_include_all)
 allocator = AngleTypeWithZeroRecordAllocator(
-    data_set, left_percentage=20, right_percentage=20,
-    zero_percentage=8, zero_left_percentage=6, zero_right_percentage=6,
+    data_set, left_percentage=35, right_percentage=35,
+    zero_percentage=6, zero_left_percentage=3, zero_right_percentage=3,
     left_right_image_offset_angle=0.25)
 generator = pipe_line_generators(
     shift_image_generator(angle_offset_pre_pixel=0.002),
@@ -15,7 +15,7 @@ generator = pipe_line_generators(
     brightness_image_generator(0.35),
     shadow_generator
 )
-data_generator = DataGenerator(allocator, generator)
+data_generator = DataGenerator(allocator.allocate, generator)
 Trainer(learning_rate=0.0001, epoch=45, dropout=0.5, multi_process=True,
         custom_name="shift_0.002_bright_0.35_angles_35_30_35").fit(
     data_generator.generate(batch_size=256),
